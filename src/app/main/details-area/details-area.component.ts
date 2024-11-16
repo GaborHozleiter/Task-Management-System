5106c09e2d01622319381f9adf33d7e5f4e2d5ab
Gabor Hozleiter <hozleitergabor@gmail.com>
2024-11-16 01:50:13 +0100

    styled the main-content

---
+++ src/app/main/details-area/details-area.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-area',
+  standalone: true,
+  imports: [],
+  templateUrl: './details-area.component.html',
+  styleUrl: './details-area.component.scss'
+})
+export class DetailsAreaComponent {
+
+}
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { DetailsAreaComponent } from './details-area/details-area.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, SidebarComponent],
+  imports: [HeaderComponent, SidebarComponent, DetailsAreaComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
